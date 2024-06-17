f4747a33790cc48c95ff23d9f41f5f8517dc4acb
Vitaly Sulimov <v.sulimov@fastmail.fm>
2024-06-18 01:23:33 +0300

    Create an Android Studio project.
    An empty Android Studio project with a library module and all the necessary configurations for further development.

---
+++ slidertabs/build.gradle.kts
@@ -0,0 +1,30 @@
+plugins {
+    alias(libs.plugins.jetbrains.kotlin.android)
+    alias(libs.plugins.android.library)
+}
+
+android {
+    namespace = "com.vsulimov.slidertabs"
+    compileSdk = 34
+
+    defaultConfig {
+        minSdk = 24
+        consumerProguardFiles("consumer-rules.pro")
+    }
+
+    buildTypes {
+        release {
+            isMinifyEnabled = false
+            proguardFiles(getDefaultProguardFile("proguard-android-optimize.txt"), "proguard-rules.pro")
+        }
+    }
+
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_1_8
+        targetCompatibility = JavaVersion.VERSION_1_8
+    }
+
+    kotlinOptions {
+        jvmTarget = "1.8"
+    }
+}
